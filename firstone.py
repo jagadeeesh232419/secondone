d5b89676f2f499025f3524b026687cbad5af482e
root <root@ip-172-31-8-14.ap-south-1.compute.internal>
2022-05-15 15:14:01 +0000

    first commit

---
+++ firstone.py
@@ -0,0 +1,12 @@
+mport random
+import  pytz
+
+name1 = 'Jagadeesh'
+
+print (pytz.all_timezones, '/n')
+
+list1 = [1, 2, 3, 4, 5, 6]
+a = random.choice(list1)
+print (a)
+print (f"Hello {name1} and {a} ")
+
